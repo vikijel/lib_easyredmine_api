9ec4c1fa5edad12a8d271317bcf5e9587d4b0b97
Viktor Jelínek <vikijel@gmail.com>
2016-05-05 17:42:53 +0200

    Load 1.9.0

---
+++ rest/easy_invoices.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * @author         EasyJoomla.org
+ * @copyright      ©2013 - 2015 EasyJoomla.org
+ * @license        http://opensource.org/licenses/LGPL-3.0 LGPL-3.0
+ * @package        Joomla
+ * @subauthor      VikiJel
+ * @version        @see easyredmine_api.xml
+ */
+defined('_JEXEC') or die('Direct access is not allowed!');
+
+jimport('easyredmine_api.rest');
+
+/**
+ * @author     VikiJel
+ * @package    Joomla
+ */
+class EasyRedmineRestApiEasyInvoices extends EasyRedmineRestApi
+{
+	/** @var string $context */
+	protected $context = 'easy_invoices';
+
+	/** @var string $context_one */
+	protected $context_one = 'easy_invoice';
+}
